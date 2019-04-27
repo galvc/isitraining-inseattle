98abdaeb393f67dd723d0fd48271c1291a5718bb
Chelsea <galvc@uw.edu>
2019-04-26 17:02:59 -0700

    finishing touches on the styling, deleting unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,127 +1,117 @@
-import React, { Component } from 'react';
-import Raining from './Raining';
-// import Background from './Background';
-import Credit from './Credit';
-// import Unsplash from 'unsplash-js';
-// import { Grommet, Heading, Box } from 'grommet';
-import styled, { css } from 'styled-components';
+import React, { Component } from "react";
+import Raining from "./Raining";
+import Credit from "./Credit";
+import styled from "styled-components";
 
-//hiding the key so it doesn't show up when committed
+//these are keys for the APIs
 const api_key = process.env.REACT_APP_WEATHER_API_KEY;
-// const Unsplash = require('unsplash-js').default;
 const appid_unsplash = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
-const axios = require('axios');
+const axios = require("axios");
 
-const Heading = styled.h1`
-        margin: 0;
-        padding-top: 2em;
-        font-size: 5em;
-      `;
-
+//styled-components
 const Background = styled.div`
-  background: url('${props => props.bgImage || "black"}');
+  background: url('${(props) => props.bgImage || "black"}');
   background-size: cover;
   min-height: 100vh;
   box-sizing: border-box;
+  padding-top: 2em;
 `;
 
 const Content = styled.div`
-  flex: 1;
-  text-align: center;
-  margin: 0;
-  padding: 4em;
-  text-align: center;
-  
+    flex: 1;
+    text-align: center;
+    margin: 0;
+    padding-top: 8em;
+`;
+
+const Heading = styled.h1`
+    margin: 0;
+    font-size: 6em;
 `;
 
 const Caption = styled.section`
-  font-size: 1em;
-  character-spacing: 10px;
+    font-size: 1em;
+    character-spacing: 10px;
 `;
 
 class App extends Component {
+    //isRaining is set to default false, even though we all know it's ALWAYS raining here
+    state = {
+        isRaining: false,
+        results: [],
+        user: {}
+    };
 
-  //isRaining is set to default false, even though we all know it's ALWAYS raining here
-  state = {
-    isRaining: false,
-    results: [],
-    user: {}
-  };
+    // loads the weather json from the api
+    loadWeather = async (e) => {
+        e.preventDefault();
 
-  // loads the weather json from the api
-  loadWeather = async (e) => {
-    e.preventDefault();
+        const api_call = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=seattle,us&appid=${api_key}`
+        );
+        const response = await api_call.json();
 
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=seattle,us&appid=${api_key}`);
-    const response = await api_call.json();
-    
-    if(response.main === "Rain"){
-      this.setState({isRaining: true});
-    }
-    console.log(response);
-  };
-
-  //this function will get the data from Unsplash using axios
-  //it will return two objects: results and user
-  //it will move those objects into state
-  //so that it can be accessed by Credit and Background components
-  async getBackgroundUnsplash() {
-    let data;
-    try{
-      data = await axios
-        .get(
-          `https://api.unsplash.com/search/photos/?query=seattle&client_id=${appid_unsplash}`
-        ).then(response => { return response })
-        .then(response => { 
-          return response.data.results[0]; 
-          //return the results of the first object that will show
-          //this will return all data of the first photo including url and user
-          //IMPROVEMENT return a random photo of seattle
-           }) 
-    } catch(err) {
-        console.log('Fetch error', err);
-      };
-
-      this.setState({
-        url: data.urls.full,
-        user: {
-          username: data.user.username,
-          url: data.user.links.self
+        if (response.main === "Rain") {
+            this.setState({ isRaining: true });
         }
-      })
-      // return url; //for testing
-  };
+        console.log(response);
+    };
 
-  componentDidMount() {
-    //this will load the background so no function needs to declare it
-    this.getBackgroundUnsplash();
-  };
-  
-  
+    //this function will get the data from Unsplash using axios
+    //it will return two objects: results and user
+    //it will move those objects into state
+    //so that it can be accessed by Credit and Background components
+    async getBackgroundUnsplash() {
+        let data;
+        try {
+            data = await axios
+                .get(
+                    `https://api.unsplash.com/search/photos/?query=seattle&client_id=${appid_unsplash}`
+                )
+                .then((response) => {
+                    return response;
+                })
+                .then((response) => {
+                    return response.data.results[0];
+                    //return the results of the first object that will show
+                    //this will return all data of the first photo including url and user
+                    //IMPROVEMENT return a random photo of seattle
+                });
+        } catch (err) {
+            console.log("Fetch error", err);
+        }
 
-  render() {
-      
+        this.setState({
+            url: data.urls.full,
+            user: {
+                username: data.user.username,
+                url: data.user.links.self
+            }
+        });
+        // return url; //for testing
+    }
 
-      
-      return (
-          <Background bgImage={this.state.url}>  
-          <Content>   
-          Is it raining in Seattle?   
-          <Heading>
-              {/* returns a phrase if isRaining is true/false */}
-              <Raining isRaining={this.state.isRaining} />
-          </Heading>
-            
-              <Caption>
-              <Credit getUser={this.state.user} />    
-              </Caption>
-          </Content>
-        </Background>
-      );
-  };
+    componentDidMount() {
+        //this will load the background so no function needs to declare it
+        this.getBackgroundUnsplash();
+    }
+
+    render() {
+        return (
+            <Background bgImage={this.state.url}>
+                <Content>
+                    Is it raining in Seattle?
+                    <Heading>
+                        {/* returns a phrase if isRaining is true/false */}
+                        <Raining isRaining={this.state.isRaining} />
+                    </Heading>
+                    <Caption>
+                        <Credit getUser={this.state.user} />
+                    </Caption>
+                </Content>
+            </Background>
+        );
+    }
 }
 
 export default App;
-
-
-
--- src/Credit.js
+++ src/Credit.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Link } from 'rebass';
+import React from "react";
+import { Link } from "rebass";
 //this function will take a prop of object user
 //it will take the user's name and url
 //user.username and user.links.html
 //then it will return a div (with style later on) containing the name with links
-function Credit (props) {
-    console.log('credit ' + props.getUser.username)
+function Credit(props) {
+    console.log("credit " + props.getUser.username);
     const { url, username } = props.getUser;
     return (
-        <span>Photo by <Link href={url}>{username}</Link> </span>
-    )
+        <span>
+            Photo by <Link href={url}>{username}</Link> on{" "}
+            <a
+                href="https://unsplash.com/?utm_source=is_it_raining_in_seattle&utm_medium=referral"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Unsplash
+            </a>
+        </span>
+    );
 }
 
 export default Credit;
